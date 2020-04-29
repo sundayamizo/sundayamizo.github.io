1651ee62ad562cad477922f4fe77eeda43d54096
sundayamizo <sundayakent@gmail.com>
2020-04-29 15:41:11 +0530

    change contact form emails

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Contact form <yourmail@domain.com>';
-$sendTo = 'your@mail.com';
+$sendTo = 'sundayakent@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
